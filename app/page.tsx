6f4df51316605bace3154afb434e4421fb048bd1
Kacper <fryt.kacper1@gmail.com>
2025-02-06 14:06:25 +0100

    Created template for login/register page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,85 @@
+"use client";
+
 export default function Login() {
+  const videoSrc = "/movies/library-movie.mp4";
+
   return (
     <main className="relative w-full h-screen overflow-hidden">
-      <div
-        className="absolute inset-0 bg-cover bg-center bg-no-repeat"
+      <video
+        className="absolute inset-0 w-full h-full object-cover"
+        src={videoSrc}
+        autoPlay
+        loop
+        muted
+        playsInline
         style={{
-          backgroundImage: "url('/images/library-image.jpeg')",
-          filter: "blur(5px)",
-          transform: "scale(1.1)",
+          filter: "blur(3px)",
+          transform: "scale(1.02)",
         }}
-      ></div>
-      <div className="absolute inset-0 bg-black opacity-20 flex justify-center items-center"></div>
-      <div className="relative z-10 flex justify-center items-center"></div>
+      ></video>
+      <div className="absolute inset-0 bg-black opacity-20"></div>
+      <div className="absolute inset-0 flex justify-center items-center">
+        <div className="bg-white p-8 rounded-3xl shadow-lg max-w-md w-full">
+          <h1 className="text-2xl font-bold mb-4">Logowanie/Rejestracja</h1>
+          <form>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="email"
+              >
+                Email
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="email"
+                type="email"
+                placeholder="Email"
+              />
+            </div>
+            <div className="mb-6">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="password"
+              >
+                Hasło
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                id="password"
+                type="password"
+                placeholder="********"
+              />
+            </div>
+            <div className="flex items-center justify-between mb-4">
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="button"
+              >
+                Zaloguj się
+              </button>
+              <button
+                className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="button"
+              >
+                Zarejestruj się
+              </button>
+            </div>
+            <div className="flex items-center justify-center">
+              <button
+                className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline flex items-center"
+                type="button"
+              >
+                <img
+                  src="/images/google-icon.svg"
+                  alt="Google Icon"
+                  className="w-5 h-5 mr-2"
+                />
+                Zaloguj się przez Google
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </main>
   );
 }
---
+++ config/firebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD2MixUN2ydKc7r1rgrxvj3or7Snqztjlo",
+  authDomain: "bookshare-app-1debf.firebaseapp.com",
+  projectId: "bookshare-app-1debf",
+  storageBucket: "bookshare-app-1debf.firebasestorage.app",
+  messagingSenderId: "680412936960",
+  appId: "1:680412936960:web:fa3d8887d81e68ec3451e5",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
